45ac73a731e9f871a78f1a03ede85a79c5689b5a
dev-sys-code <kaydenbuster@outlook.com>
2026-07-29 22:21:44 +0100

    added some languages

--- src/currencies/currency.cpp
+++ src/currencies/currency.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
 #include "headers/currency.hpp"
+
+void currency(const std::string currency) {
+    if (currency == "Russian ruble" || currency == "Russian Ruble") {}
+    else if (currency == "Afghan afgani" || currency == "Afghan Afgani") {}
+    else if (currency == "Euro" || currency == "euro") {}
+    else if (currency == "Albanian lek" || currency == "Albanian Lek") {}
+    else if (currency == "Algerian dinar" || currency == "Algerian Dinar") {}
+    else if (currency == "Angolan kwanza" || currency == "Angolan Kwanza") {}
+    else if (currency == "Eastern Caribbean dollar" || currency == "Eastern Caribbean Dollar") {}
+    else if (currency == "Argentine peso" || currency == "Argentine Peso") {}
+    else if (currency == "Armenian dram" || currency == "Armenian Dram") {}
+    else if (currency == "Aruban florin" || currency == "Aruban Florin") {}
+    else if (currency == "Saint Helena pound" || currency == "Saint Helena Pound") {}
+    else if (currency == "Australian dollar" || currency == "Australian Dollar") {}
+    else if (currency == "Azerbaijani manat" || currency == "Azerbaijani Manat") {}
+    else if (currency == "Bahamian dollar" || currency == "Bahamian Dollar") {}
+    else if (currency == "Bahraini dinar" || currency == "Bahraini Dinar") {}
+    else if (currency == "Bangladeshi taka" || currency == "Bangladeshi Taka") {}
+    else if (currency == "Barbadian dollar" || currency == "Barbadian Dollar") {}
+}
